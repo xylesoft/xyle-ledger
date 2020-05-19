e37cab664321d107aabf8cc13e8fa38df1f66ca1
Jeramy Wenserit <jeramy@digidip.net>
2020-05-19 23:35:45 +0200

    add postinstall to package.json

--- src/__tests__/sign-transaction.test.ts
+++ src/__tests__/sign-transaction.test.ts
@@ -6,24 +6,36 @@ import signTransaction from '../sign-transaction';
 
 describe('Testing for signing of transactions', () => {
     // secondary ledger for checking error guards
-    const { transaction, publicKey, privateKey } = createRootTransaction(0, ledgerTimestamp(), 'homer', 'My Test Ledger');
+    const { transaction, publicKey, privateKey } = createRootTransaction(
+        0,
+        ledgerTimestamp(),
+        'homer',
+        'My Test Ledger'
+    );
 
     it('should throw a verification error due to key mistmatch on root transaction', () => {
-        const { transactions: lTrans } = mockChain([25.99, 11.0, 54.11, -44.43], 'homer');
+        const { transactions: lTrans } = mockChain([25.99, 11, 54.11, -44.43], 'homer');
         expect(() => verifyLedger(lTrans, publicKey)).toThrowError(/Signature error:.+/);
     });
 
     it('should be able to sign transactions', () => {
         const { transactions: lTrans, publicKey: pubKey, privateKey: privKey } = mockChain(
-            [25.99, 11.0, 54.11, -44.43],
+            [25.99, 11, 54.11, -44.43],
             'homer'
         );
 
-        lTrans.push(signTransaction({
-            value: 7.99,
-            reference: 'Manual sign transaction',
-            timestamp: ledgerTimestamp(),
-        }, lTrans[lTrans.length - 1], privKey, 'homer'))
+        lTrans.push(
+            signTransaction(
+                {
+                    value: 7.99,
+                    reference: 'Manual sign transaction',
+                    timestamp: ledgerTimestamp()
+                },
+                lTrans[lTrans.length - 1],
+                privKey,
+                'homer'
+            )
+        );
 
         expect(verifyLedger(lTrans, pubKey)).toBeTruthy();
     });
